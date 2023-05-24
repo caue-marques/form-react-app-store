8cf8410da4472021a6aa3a08aed56767fbf8cb27
caue-marques <caue.marques@acct.global>
2023-05-24 12:36:06 -0300

    Refactor

--- react-app-template/react/components/SearchSKUForm.tsx
+++ react-app-template/react/components/SearchSKUForm.tsx
@@ -1,148 +1,248 @@
-import React, { Fragment, useState } from "react"
+import React, { FormEvent, Fragment, useState } from 'react'
 import { useProduct } from 'vtex.product-context'
+import { useQuery } from 'react-apollo'
+
+import GET_SEARCHRECIPE from '../graphql/queries/GET_SEARCHRECIPE.gql'
+
+interface DocumentsType {
+  id: string
+  fields: Array<{
+    key: string
+    value: string
+  }>
+}
+interface Documents {
+  documents: DocumentsType[]
+}
 
 const SearchSKUForm: StorefrontFunctionComponent = () => {
+  const { data, loading, refetch } = useQuery<Documents>(GET_SEARCHRECIPE, {
+    ssr: false,
+  })
 
-    const defaultProduct = {
-        'id_sku': '',
-        'titulo': '',
-        'data': '',
-        'conteudo': '',
-        'produtos': 0
+  const productContextValue = useProduct()
+
+  const emptyFields = {
+    id: '',
+    id_sku: '',
+    titulo: '',
+    data: '',
+    conteudo: '',
+    produtos: '',
+  }
+
+  const [product, setProduct] = useState({})
+
+  const [id, setId] = useState('')
+  const [idNotSent, setIdNotSent] = useState(false)
+  const [notFound, setNotFound] = useState(false)
+
+  const resetProduct = () => {
+    setProduct(emptyFields)
+  }
+
+  const resetStatus = () => {
+    setIdNotSent(false)
+    setNotFound(false)
+  }
+
+  const searchSku = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    await refetch()
+
+    resetProduct()
+    resetStatus()
+
+    if (!id) {
+      setIdNotSent(true)
+
+      return
     }
 
-    const productContextValue = useProduct()
-    console.log(productContextValue)
-
-    const [product, setProduct] = useState(defaultProduct)
-    const [id, setId] = useState('')
-    const [idNotSent, setIdNotSent] = useState(false)
-    const [notFound, setNotFound] = useState(false)
-
-    const resetProduct = () => {
-        setProduct(defaultProduct)
+    if (!data || loading) {
+      return
     }
 
-    const resetStatus = () => {
-        setIdNotSent(false)
-        setNotFound(false)
-    }
-
-    const searchSku = async (e: any) => {
-        e.preventDefault()
-
-        resetProduct()
-        resetStatus()
-
-        let idNotSent = false
-
-        if (!id) idNotSent = true
-
-        setIdNotSent(idNotSent)
-
-        if (idNotSent) return
-
-        let searchProduct = defaultProduct
-        let productFound = false
-
-        await fetch(`/api/dataentities/Receitas/search/?_fields=id_sku,titulo,data,conteudo,produtos`)
-            .then(data => {
-                return data.json()
-            })
-            .then(skus => {
-                for (let sku of skus) {
-                    if (sku.produtos == id) {
-                        searchProduct = sku
-                        productFound = true
-                        idNotSent = false
-                        break
-                    }
-                }
-
-                setProduct(searchProduct)
-                setNotFound(!productFound)
-                setIdNotSent(idNotSent)
-            })
-    }
-
-    const searchMostRecentSku = async (e: any) => {
-        e.preventDefault()
-
-        resetProduct()
-        resetStatus()
-
-        let finalMostRecentReceipe = defaultProduct
-
-        await fetch(`/api/dataentities/Receitas/search/?_fields=id_sku,titulo,data,conteudo,produtos`)
-            .then(data => {
-                return data.json()
-            })
-            .then(skus => {
-                let mostRecentReceipe = skus[0]
-                let dateMostRecente = new Date(mostRecentReceipe.data)
-
-                for (let sku of skus) {
-                    let currentDate = new Date(sku.data)
-
-                    if (currentDate > dateMostRecente) {
-                        dateMostRecente = currentDate
-                        finalMostRecentReceipe = sku
-                    }
-                }
-
-                setProduct(finalMostRecentReceipe)
-            })
-    }
-
-    return (
-        <div>
-            <form className="flex justify-center items-center mt6">
-                <input className="w-25 h2 ba b--black-80" type="text" value={id} name="id_sku" id="id_sku" placeholder="ID DO SKU" onChange={(e) => setId(e.target.value)} />
-                <button type="submit" className="pointer ml5 h2 bg-green ba b--black-10" onClick={searchSku}>Buscar Receita</button>
-                <button type="submit" className="pointer ml5 h2 bg-blue ba b--black-10" onClick={searchMostRecentSku}>Buscar Receita Mais Recente</button>
-            </form>
-
-            {idNotSent && <div className="red w-50 mt7 center tc b">Não é possível buscar uma receita sem informar o ID</div>}
-            {notFound && <div className="red w-50 mt7 center tc b">Produto não encontrado para o ID informado</div>}
-
-
-            <table className="center mt6 w-75">
-                <thead className="f4 lh-copy">
-                    <tr>
-                        <th className="pr7">ID</th>
-                        <th className="pr7">TITULO</th>
-                        <th className="pr7">DATA</th>
-                        <th className="pr7">CONTEUDO</th>
-                        <th className="pr7">SKU</th>
-                    </tr>
-                </thead>
-                {product.id_sku &&
-                    <tbody>
-                        <tr className="tc">
-                            <td className="pr7">{product.id_sku || ''}</td>
-                            <td className="pr7">{product.titulo || ''}</td>
-                            <td className="pr7">{product.data || ''}</td>
-                            <td className="pr7">{product.conteudo || ''}</td>
-                            <td className="pr7">{product.produtos || ''}</td>
-                        </tr>
-                    </tbody>
-                }
-                {productContextValue &&
-                    <Fragment>
-                        <tbody>
-                            <tr className="tc">
-                                <td className="pr7">{productContextValue?.product?.productId}</td>
-                                <td className="pr7">{productContextValue?.product?.productName}</td>
-                                <td className="pr7">{productContextValue?.product?.description}</td>
-                                <td className="pr7">{productContextValue?.product?.description}</td>
-                                <td className="pr7">{productContextValue?.selectedItem?.itemId}</td>
-                            </tr>
-                        </tbody>
-                    </Fragment>
-                }
-            </table>
-        </div>
+    const productFinded = data.documents.find(
+      (doc) => doc.fields[1].value === id
     )
-};
 
-export default SearchSKUForm;
+    if (!productFinded) {
+      setNotFound(true)
+
+      return
+    }
+
+    const findFieldValue = (key: string) => {
+      return (
+        productFinded.fields.find((productFields) => productFields.key === key)
+          ?.value ?? ''
+      )
+    }
+
+    const extractedFields = {
+      id: findFieldValue('id'),
+      id_sku: findFieldValue('id_sku'),
+      titulo: findFieldValue('titulo'),
+      data: findFieldValue('data'),
+      conteudo: findFieldValue('conteudo'),
+      produtos: findFieldValue('produtos'),
+    }
+
+    setProduct(extractedFields)
+
+    // await fetch(`/api/dataentities/Receitas/search/?_fields=id_sku,titulo,data,conteudo,produtos`)
+    //     .then(data => {
+    //         return data.json()
+    //     })
+    //     .then(skus => {
+    //         for (let sku of skus) {
+    //             if (sku.produtos == id) {
+    //                 searchProduct = sku
+    //                 productFound = true
+    //                 idNotSent = false
+    //                 break
+    //             }
+    //         }
+
+    //         setProduct(searchProduct)
+    //         setNotFound(!productFound)
+    //         setIdNotSent(idNotSent)
+    //     })
+  }
+
+  // const searchMostRecentSku = async (e: any) => {
+  //   e.preventDefault()
+
+  //   resetProduct()
+  //   resetStatus()
+
+  //   const finalMostRecentReceipe = defaultProduct
+
+  //   const mostRecentReceipe = data.documents[0].fields
+  //   let dateMostRecente = new Date(mostRecentReceipe[3].value)
+
+  //   for (let i = 0; i < data.documents.length; i++) {
+  //     const currentDate = new Date(data.documents[i].fields[3].value)
+
+  //     if (currentDate > dateMostRecente) {
+  //       dateMostRecente = currentDate
+  //       finalMostRecentReceipe.id_sku = data.documents[i].fields[1].value
+  //       finalMostRecentReceipe.titulo = data.documents[i].fields[2].value
+  //       finalMostRecentReceipe.data = data.documents[i].fields[3].value
+  //       finalMostRecentReceipe.conteudo = data.documents[i].fields[4].value
+  //       finalMostRecentReceipe.produtos = data.documents[i].fields[5].value
+  //     }
+  //   }
+
+  //   setProduct(finalMostRecentReceipe)
+
+  //   // await fetch(`/api/dataentities/Receitas/search/?_fields=id_sku,titulo,data,conteudo,produtos`)
+  //   //     .then(data => {
+  //   //         return data.json()
+  //   //     })
+  //   //     .then(skus => {
+  //   //         let mostRecentReceipe = skus[0]
+  //   //         let dateMostRecente = new Date(mostRecentReceipe.data)
+
+  //   //         for (let sku of skus) {
+  //   //             let currentDate = new Date(sku.data)
+
+  //   //             if (currentDate > dateMostRecente) {
+  //   //                 dateMostRecente = currentDate
+  //   //                 finalMostRecentReceipe = sku
+  //   //             }
+  //   //         }
+
+  //   //         setProduct(finalMostRecentReceipe)
+  //   //     })
+  // }
+
+  return (
+    <div>
+      <form
+        className="flex justify-center items-center mt6"
+        onSubmit={searchSku}
+      >
+        <input
+          className="w-25 h2 ba b--black-80"
+          type="text"
+          value={id}
+          name="id_sku"
+          id="id_sku"
+          placeholder="ID DO SKU"
+          onChange={(e) => setId(e.target.value)}
+        />
+        <button className="pointer ml5 h2 bg-green ba b--black-10">
+          Buscar Receita
+        </button>
+        {/* <button
+          type="submit"
+          className="pointer ml5 h2 bg-blue ba b--black-10"
+          onClick={searchMostRecentSku}
+        >
+          Buscar Receita Mais Recente
+        </button> */}
+      </form>
+
+      {idNotSent && (
+        <div className="red w-50 mt7 center tc b">
+          Não é possível buscar uma receita sem informar o ID
+        </div>
+      )}
+      {notFound && (
+        <div className="red w-50 mt7 center tc b">
+          Produto não encontrado para o ID informado
+        </div>
+      )}
+
+      <table className="center mt6 w-75">
+        <thead className="f4 lh-copy">
+          <tr>
+            <th className="pr7">ID</th>
+            <th className="pr7">TITULO</th>
+            <th className="pr7">DATA</th>
+            <th className="pr7">CONTEUDO</th>
+            <th className="pr7">SKU</th>
+          </tr>
+        </thead>
+        {product.id_sku && (
+          <tbody>
+            <tr className="tc">
+              <td className="pr7">{product.id_sku}</td>
+              <td className="pr7">{product.titulo}</td>
+              <td className="pr7">{product.data}</td>
+              <td className="pr7">{product.conteudo}</td>
+              <td className="pr7">{product.produtos}</td>
+            </tr>
+          </tbody>
+        )}
+        {productContextValue && (
+          <Fragment>
+            <tbody>
+              <tr className="tc">
+                <td className="pr7">
+                  {productContextValue?.product?.productId}
+                </td>
+                <td className="pr7">
+                  {productContextValue?.product?.productName}
+                </td>
+                <td className="pr7">
+                  {productContextValue?.product?.description}
+                </td>
+                <td className="pr7">
+                  {productContextValue?.product?.description}
+                </td>
+                <td className="pr7">
+                  {productContextValue?.selectedItem?.itemId}
+                </td>
+              </tr>
+            </tbody>
+          </Fragment>
+        )}
+      </table>
+    </div>
+  )
+}
+
+export default SearchSKUForm
--- react-app-template/react/components/SendReviewForm.tsx
+++ react-app-template/react/components/SendReviewForm.tsx
@@ -1,101 +1,187 @@
-import React, { useState } from "react"
+import { useMutation } from 'react-apollo'
 import { useProduct } from 'vtex.product-context'
+import React, { FormEvent, useState } from 'react'
+
+import SendReview from '../graphql/queries/sendFormReview.gql'
 
 const SendReviewForm: StorefrontFunctionComponent = () => {
+  const productContextValue = useProduct()
 
-    const productContextValue = useProduct()
+  const [data] = useMutation(SendReview)
 
-    //const [produto, setProduto] = useState('');
-    const [data, setData] = useState('');
-    const [usuario, setUsuario] = useState('');
-    const [nota, setNota] = useState('');
-    const [comentario, setComentario] = useState('');
-    const [sucesso, setSucesso] = useState(false)
-    const [produtoVazio, setProdutoVazio] = useState(false)
-    const [dataVazia, setDataVazia] = useState(false)
-    const [usuarioVazio, setUsuarioVazio] = useState(false)
-    const [notaVazia, setNotaVazia] = useState(false)
+  // utilizar caso não seja utilizado o useProduct()
 
-    const sendReview = async (e: any) => {
-        e.preventDefault()
-        setSucesso(false)
+  // const [produto, setProduto] = useState('');
 
-        let productIsEmpty = false
-        let dataIsEmpty = false
-        let usuarioIsEmpty = false
-        let notaIsEmpty = false
+  const [date, setDate] = useState('')
+  const [usuario, setUsuario] = useState('')
+  const [nota, setNota] = useState('')
+  const [comentario, setComentario] = useState('')
+  const [sucesso, setSucesso] = useState(false)
+  const [produtoVazio, setProdutoVazio] = useState(false)
+  const [dataVazia, setDataVazia] = useState(false)
+  const [usuarioVazio, setUsuarioVazio] = useState(false)
+  const [notaVazia, setNotaVazia] = useState(false)
 
-        // if (!produto)
-        //     productIsEmpty = true
+  const sendReview = async (e: FormEvent) => {
+    e.preventDefault()
+    setSucesso(false)
 
-        if (!data)
-            dataIsEmpty = true
+    const productIsEmpty = false
+    let dataIsEmpty = false
+    let usuarioIsEmpty = false
+    let notaIsEmpty = false
 
-        if (!usuario)
-            usuarioIsEmpty = true
+    // utilizar caso não seja utilizado o useProduct()
 
-        if (!nota)
-            notaIsEmpty = true
+    // if (!produto)
+    //     productIsEmpty = true
 
-        setProdutoVazio(productIsEmpty)
-        setDataVazia(dataIsEmpty)
-        setUsuarioVazio(usuarioIsEmpty)
-        setNotaVazia(notaIsEmpty)
-            
-        if(!data || !usuario ||!nota) return
+    if (!date) dataIsEmpty = true
 
-        await fetch(`/api/dataentities/Reviews/documents?_schema=Reviews`, {
-            headers: {
-                "Content-Type": "application/json"
-            },
-            method: "POST",
-            body: JSON.stringify({
-                'produto': productContextValue?.selectedItem?.itemId,
-                'data': data,
-                'usuario': usuario,
-                'nota': nota,
-                'comentario': comentario
-            })
-        })
-            .then(data => {
-                return data.json()
-            })
-            .then(() => {
-                setSucesso(true)
-            })
-    }
+    if (!usuario) usuarioIsEmpty = true
 
-    return (
-        <div>
-            <form className="mt7 w-50 flex center flex-column items-center">
-                <label className="db mb2" htmlFor="sku">Produto avaliado:</label>
-                <input className="w-25 h2 ba b--black-80 db mb2" type="text" value={productContextValue?.selectedItem?.itemId} name="sku" id="sku" placeholder="Produto" disabled={true}/>
-                {produtoVazio && <div className="f6 lh-copy tc red b">Campo Vazio</div>}
+    if (!nota) notaIsEmpty = true
 
-                <label className="db mb2" htmlFor="data">Data:</label>
-                <input className="db mb2 w-25 h2 ba b--black-80" type="date" value={data} name="data" id="data" placeholder="Data da avaliação" onChange={(e) => setData(e.target.value)} />
-                {dataVazia && <div className="f6 lh-copy tc  red b">Campo Vazio</div>}
+    setProdutoVazio(productIsEmpty)
+    setDataVazia(dataIsEmpty)
+    setUsuarioVazio(usuarioIsEmpty)
+    setNotaVazia(notaIsEmpty)
 
-                <label className="db mb2" htmlFor="sku">Usuário:</label>
-                <input className="db mb2 w-25 h2 ba b--black-80" type="text" value={usuario} name="usuario" id="usuario" placeholder="Usuário" onChange={(e) => setUsuario(e.target.value)} />
-                {usuarioVazio && <div className="f6 lh-copy tc  red b">Campo Vazio</div>}
+    // incluir !produto caso não seja utilizado o useProduct()
+    if (!date || !usuario || !nota) return
 
-                <label className="db mb2" htmlFor="sku">Nota:</label>
-                <input className="db mb2 w-25 h2 ba b--black-80" type="number" value={nota} name="nota" id="nota" placeholder="Nota" onChange={(e) => setNota(e.target.value)} />
-                {notaVazia && <div className="f6 lh-copy tc red b">Campo Vazio</div>}
+    data({
+      variables: {
+        dataEntity: 'Reviews',
+        account: 'estagioacct',
+        schema: 'Reviews',
+        document: {
+          document: {
+            userName: usuario,
+            data: date,
+            nota,
+            comentario,
+            produto: productContextValue?.selectedItem?.itemId,
+          },
+        },
+      },
+    }).then((dados) => {
+      setSucesso(true)
+    })
 
-                <label className="db mb2" htmlFor="sku">Comentário:</label>
-                <input className="db mb2 w-25 h2 ba b--black-80" type="textarea" value={comentario} name="comentario" id="comentario" placeholder="Comentário" onChange={(e) => setComentario(e.target.value)} />
+    // await fetch(`/api/dataentities/Reviews/documents?_schema=Reviews`, {
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //         //utilizar produto caso não seja utilizad o useProduct()
+    //         'produto': productContextValue?.selectedItem?.itemId,
+    //         'data': date,
+    //         'usuario': usuario,
+    //         'nota': nota,
+    //         'comentario': comentario
+    //     })
+    // })
+    //     .then(data => {
+    //         return data.json()
+    //     })
+    //     .then(() => {
+    //         setSucesso(true)
+    //     })
+  }
 
-                <button type="submit" className="mt6 pointer ml5 h2 bg-green ba b--black-10" onClick={sendReview}>Enviar Review</button>
-            </form>
+  return (
+    <div>
+      <form className="mt7 w-50 flex center flex-column items-center">
+        <label className="db mb2" htmlFor="sku">
+          Produto avaliado:
+        </label>
+        <input
+          className="w-25 h2 ba b--black-80 db mb2"
+          type="text"
+          value={productContextValue?.selectedItem?.itemId}
+          name="sku"
+          id="sku"
+          placeholder="Produto"
+          disabled
+        />
+        {produtoVazio && <div className="f6 lh-copy tc red b">Campo Vazio</div>}
 
-            <div>
-                {sucesso && <div className="tc mt5 blue b">Review enviada com sucesso</div>}
-            </div>
-        </div>
+        <label className="db mb2" htmlFor="data">
+          Data:
+        </label>
+        <input
+          className="db mb2 w-25 h2 ba b--black-80"
+          type="date"
+          value={date}
+          name="data"
+          id="data"
+          placeholder="Data da avaliação"
+          onChange={(e) => setDate(e.target.value)}
+        />
+        {dataVazia && <div className="f6 lh-copy tc  red b">Campo Vazio</div>}
 
-    )
-};
+        <label className="db mb2" htmlFor="sku">
+          Usuário:
+        </label>
+        <input
+          className="db mb2 w-25 h2 ba b--black-80"
+          type="text"
+          value={usuario}
+          name="usuario"
+          id="usuario"
+          placeholder="Usuário"
+          onChange={(e) => setUsuario(e.target.value)}
+        />
+        {usuarioVazio && (
+          <div className="f6 lh-copy tc  red b">Campo Vazio</div>
+        )}
 
-export default SendReviewForm;
+        <label className="db mb2" htmlFor="sku">
+          Nota:
+        </label>
+        <input
+          className="db mb2 w-25 h2 ba b--black-80"
+          type="number"
+          value={nota}
+          name="nota"
+          id="nota"
+          placeholder="Nota"
+          onChange={(e) => setNota(e.target.value)}
+        />
+        {notaVazia && <div className="f6 lh-copy tc red b">Campo Vazio</div>}
+
+        <label className="db mb2" htmlFor="sku">
+          Comentário:
+        </label>
+        <input
+          className="db mb2 w-25 h2 ba b--black-80"
+          type="textarea"
+          value={comentario}
+          name="comentario"
+          id="comentario"
+          placeholder="Comentário"
+          onChange={(e) => setComentario(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="mt6 pointer ml5 h2 bg-green ba b--black-10"
+          onClick={sendReview}
+        >
+          Enviar Review
+        </button>
+      </form>
+
+      <div>
+        {sucesso && (
+          <div className="tc mt5 blue b">Review enviada com sucesso</div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default SendReviewForm
